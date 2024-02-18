407890318245be6b217a83d7e687d145c18b097a
Sebastian Gil <jsgilr@unal.edu.co>
2024-02-18 01:20:08 -0500

    Update page.tsx

--- src/app/(home)/chart/page.tsx
+++ src/app/(home)/chart/page.tsx
@@ -1,10 +1,10 @@
-// import Chart from "@/components/Charts/page";
-// import React from "react";
+import Chart from "@/components/Charts/page";
+import React from "react";
 
-// const BasicChartPage: React.FC = () => {
-//   return (
-//     <Chart />
-//   );
-// };
+const BasicChartPage: React.FC = () => {
+  return (
+    <Chart />
+  );
+};
 
-// export default BasicChartPage;
+export default BasicChartPage;
